26da3419876e636f6198afc451126b78cbd26208
minsuKang <minsu910725@gmail.com>
2022-04-30 14:02:51 +0900

    Add: typescript in redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import Greetings from './Greetings';
+import ReducerSample from './ReducerSample';
 
-const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  return <Greetings name="Hello" onClick={onClick} />;
-};
-
-export default App;
+export default function App() {
+  return <ReducerSample />;
+}
